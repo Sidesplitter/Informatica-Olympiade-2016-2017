9cdc597cfeaf5a5189d169cc84805f7f38a9ebb3
Jord Nijhuis <jord@nijhuis.me>
2016-11-17 20:17:08 +0100

    Fix the input of C1.
    Instead of retrieving 10 numbers, it should retrieve 10 gaussian numbers.
    So 20 numbers.
    
    Signed-off-by: Jord Nijhuis

--- src/C1/main.cpp
+++ src/C1/main.cpp
@@ -5,20 +5,27 @@ int main() {
 
 #ifdef HUMAN_MESSAGES
     std::cout << "==== C1 ====" << std::endl
-              << "This program expects a list of 10 numbers and returns 'ja' (yes) or 'nee' (no) depending on whether "
-              << "the number is a prime or not." << std::endl;
+              << "This program expects a list of 10 gaussian numbers and returns 'ja' (yes) or 'nee' (no) depending on "
+                      "whether the number is a gaussian prime or not. It expects two numbers separated by a space for "
+                      "every gaussian number." << std::endl;
 #endif
 
-    int numbers[10] = {};
+    Point numbers[10] = {};
+
 
     // Capture ten numbers
     for (int i = 0; i < 10; i++) {
 
 #ifdef HUMAN_MESSAGES
 
-        printf("Number %d: ", i + 1);
+        printf("Guassian number %d: ", i + 1);
 #endif
-        std::cin >> numbers[i];
+        int x, y;
+
+        std::cin >> x;
+        std::cin >> y;
+
+        numbers[i] = Point(x, y);
     }
 
     CCore cCore = CCore();
@@ -28,9 +35,9 @@ int main() {
     {
 
 #ifdef HUMAN_MESSAGES
-    printf("Number %d (%d): ", i + 1, numbers[i]);
+    printf("Number %d (%d, %d): ", i + 1, numbers[i].getX(), numbers[i].getY());
 #endif
-        if(cCore.isPrime((uint32_t) numbers[i]))
+        if(cCore.getPrimalityTester()->isGaussianPrime(numbers[i]))
         {
             std::cout << "ja" << std::endl;
         }else{
